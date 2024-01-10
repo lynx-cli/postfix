d905f65b52088de286355d4c28eeec1c492a5ec2
revi-n <150216440+revi-n@users.noreply.github.com>
2024-01-11 01:03:23 +0600

    Create bind9.sh
    ....

---
+++ bind9.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+if [ "$EUID" -ne 0 ]; then
+    echo "REQUIRES SUDO PRIVILEGES"
+    exit 1
+fi
+echo -e "\033[0;32m# CONFIGURING BIND9:\033[0m"
+apt install bind9 bind9utils bind9-doc
+curl -o /usr/share/dns/root.hints https://www.internic.net/domain/named.root
+cat <<EOL > /etc/default/named
+RESOLVCONF=no
+OPTIONS="-u bind -4"
+EOL
+sleep 2
+systemctl restart bind9
+echo -e "\033[0;32mOK\033[0m"
